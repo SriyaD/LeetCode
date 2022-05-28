0557f4ebac6dc158a4e10b7b8995c4307757d3e2
SriyaD <71544654+SriyaD@users.noreply.github.com>
2022-05-28 09:28:19 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (82.50%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,14 @@
+// 1^2  ^4
+// ^1^2^3^4
+class Solution {
+   public int missingNumber(int[] nums) { //xor
+    int res = nums.length;
+    for(int i=0; i<nums.length; i++){
+        res ^= i;
+        res ^= nums[i];
+    }
+    return res;
+}
+}
+
+
